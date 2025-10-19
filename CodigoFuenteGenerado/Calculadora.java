2c6b1e38b03b139bcbd4b1bd1d7dae6f78b9a166
IgnacioNovoa1 <ignacionovoa8@gmail.com>
2025-10-18 22:21:47 -0300

    Agrege codigo generado por VisualParadigm y Diagrama UML

---
+++ CodigoFuenteGenerado/Calculadora.java
@@ -0,0 +1,28 @@
+public class Calculadora {
+	private int _n1;
+	private int _n2;
+
+	public Calculadora() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Calculadora(int aN1, int aN2) {
+		throw new UnsupportedOperationException();
+	}
+
+	public int sumar() {
+		throw new UnsupportedOperationException();
+	}
+
+	public int multiplicar() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setN1(int aN) {
+		this._n1 = aN;
+	}
+
+	public void setN2(int aN) {
+		this._n2 = aN;
+	}
+}
---
+++ CodigoFuenteGenerado/CarroCompra.java
@@ -0,0 +1,19 @@
+public class CarroCompra {
+	private int[][] _productos;
+
+	public CarroCompra() {
+		throw new UnsupportedOperationException();
+	}
+
+	private int calcularTotal() {
+		throw new UnsupportedOperationException();
+	}
+
+	private int subTotal() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarTotal() {
+		throw new UnsupportedOperationException();
+	}
+}
